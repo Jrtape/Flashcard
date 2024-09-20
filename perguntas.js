3ab1ef05a2b547be167478bf65f752573de4c920
Jrtape <162441976+Jrtape@users.noreply.github.com>
2024-09-20 11:42:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Jogos',
+    'Jogo Mais vendido do mundo?',
+    'Minecraft'
+)
+
+criaCartao(
+    'Console',
+    'Console mais vendido do mundo?',
+    'PlayStation 2'
+)
+
+criaCartao(
+    'Jogos Antigos',
+    'Primeiro Jogo Feito?',
+    'Tennis for Two'
+)
+
+criaCartao(
+    'FPS',
+    'Primeio jogo de fps criado?',
+    'Wolfenstein 3D'
+)
+
+criaCartao(
+    'Idade',
+    'Idade do Jogo Pong',
+    '50'
+)
+
+criaCartao(
+    'Venda Console',
+    'Jogo mais vendido do xbox',
+    'Minecraft'
+)
+
+criaCartao(
+    'BattleRoyale',
+    'Qual jogo popular introduziu o modo Battle Royale?',
+    'PUBG'
+)
+
+criaCartao(
+    'Zelda',
+    'Em qual ano foi lançado "The Legend of Zelda: Ocarina of Time"?',
+    '1998 para o Nintendo 64'
+)
+
+criaCartao(
+    'Nintendo',
+    'Qual é o nome do famoso encanador que é o protagonista da série de jogos da Nintendo?',
+    'Mario'
+)
+
+criaCartao(
+    'Rockstar',
+    'Qual jogo da Rockstar é famoso por seu mundo aberto e vida de gangster?',
+    'Grand Theft Auto V'
+)
+
+criaCartao(
+    'Zelda',
+    'Qual é o nome do herói que enfrenta Ganon na série "The Legend of Zelda"?',
+    'Link'
+)
+
+criaCartao(
+    'FPS',
+    'Qual jogo de tiro em primeira pessoa é famoso por seu modo competitivo e mapas variados?',
+    'Counter-Strike: Global Offensive'
+)
